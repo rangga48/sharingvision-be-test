1a7bf2ac7b962561de223dbd3f7e607577800d2c
rangga48 <ranggaputra681@gmail.com>
2026-08-24 18:52:31 +0700

    feat: cors untuk frontend

--- main.go
+++ main.go
@@ -13,6 +13,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	_ = godotenv.Load()
 
@@ -26,6 +42,7 @@ func main() {
 	controller := controllers.NewArticleController(service)
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
 	r.POST("/article/", controller.CreateArticle)
 	r.POST("/article", controller.CreateArticle)
